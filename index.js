c30b357c0c9348f540a704c8e139e9714a48721f
Bryan Enríquez Jiménez <164971338+bryanej10@users.noreply.github.com>
2026-05-22 10:49:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,41 @@ const FormData = require("form-data");
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.json({ limit: "20mb" }));
 app.use(cors());
 
+// HEALTHCHECK
+app.get("/", (req, res) => {
+  res.send("API OK");
+});
+
 app.post("/upload", async (req, res) => {
+
   try {
-    const { fileName, fileContent } = req.body;
+
+    const {
+      url,
+      fileName,
+      fileContent
+    } = req.body;
+
+    // VALIDACIONES
+    if (!url || !fileName || !fileContent) {
+
+      return res.status(400).json({
+        error: "Missing parameters"
+      });
+    }
 
     const buffer = Buffer.from(fileContent, "base64");
 
     const form = new FormData();
+
     form.append("file", buffer, fileName);
 
     const response = await axios.post(
-      "https://latam-demo.esignanywhere.net/Api/v6/file/upload",
+      url,
       form,
       {
         headers: {
@@ -30,11 +51,24 @@ app.post("/upload", async (req, res) => {
     res.json(response.data);
 
   } catch (error) {
-    console.error(error.response?.data || error.message);
-    res.status(500).json(error.response?.data || { error: "Upload failed" });
+
+    console.error(
+      error.response?.data || error.message
+    );
+
+    res.status(500).json(
+      error.response?.data || {
+        error: "Upload failed"
+      }
+    );
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// PORT RAILWAY
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
+
 });
